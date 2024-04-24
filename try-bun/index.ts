1b614e42be1f7bab6a744103a4675ab1f9304b4b
Deveshwar Jaiswal <deveshwar.jaiswal996@gmail.com>
2024-04-24 16:09:23 +0530

    feat: Watch vs Hot

--- try-bun/index.ts
+++ try-bun/index.ts
@@ -1,1 +1,18 @@
 console.log("Hello via Bun! 1st project");
+console.log('watch mode restarts the application');
+console.log('hot mode does not restart the application, stateful objects are not lost');
+
+
+declare global {
+    var count: number;
+}
+
+var localcount: number;
+
+globalThis.count ??= 0;
+localcount ??= 0;
+
+console.log("reloaded times: ", globalThis.count, "localcount: ", localcount);
+
+globalThis.count++;
+localcount++;
