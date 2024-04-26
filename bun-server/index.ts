2a75152673997fa90f95604258b9e7d63f3f5fed
Deveshwar Jaiswal <deveshwar.jaiswal996@gmail.com>
2024-04-26 15:23:13 +0530

    feat: Learning server stuff

--- bun-server/index.ts
+++ bun-server/index.ts
@@ -1,1 +1,30 @@
-console.log("Hello via Bun!");
+const product = {
+    pcode: 1111,
+    pname: 'Apple'
+}
+
+const server = Bun.serve({
+    port: 2000,
+    fetch(req, server) {
+        // console.log(req);
+        // console.log(server);
+        // setTimeout(() => {
+        //     server.stop()
+        // }, 3000);
+        const url = new URL(req.url);
+        console.log(url)
+        switch (url.pathname) {
+            case "/products":
+                return new Response(JSON.stringify(product), {
+                    headers: {
+                        'Content-Type': 'application/json'
+                    }
+                });
+            //return new Response('Products Route: ' + req.url);
+            default:
+                return new Response("Hello Server via Bun!" + req.url);
+        }
+    }
+});
+
+console.log(`Listening on ${server.port}`);
