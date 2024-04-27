b6236683733ebab348002fe59e24cbb5765f6a6b
Deveshwar Jaiswal <deveshwar.jaiswal996@gmail.com>
2024-04-27 15:16:07 +0530

    feat: Started express => get req, wildcards, params

--- bun-express/index.ts
+++ bun-express/index.ts
@@ -1,1 +1,36 @@
-console.log("Hello via Bun!");
+import express, { type Request, type Response } from "express";
+
+const app = express();
+
+const PORT = 2000;
+const data = {
+    pCode: 1000,
+    pName: 'Apple',
+    price: 20
+}
+app.get('/', (req: Request, res: Response) => {
+    res.setHeader('Content-Type', 'application/json');
+    res.send(data);
+});
+
+app.get('/products*', (req: Request, res: Response) => {
+    //wildcard routes + * ? //
+    res.setHeader('Content-Type', 'text/html');
+    res.send(`<h1>Hello Products Route!</h1> <br> <h2>Url: ${req.url}</h2> `);
+});
+
+app.get('/users/:userId/:userName', (req: Request, res: Response) => {
+    //params
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html> <head> <title>Bun + Express</title></head>');
+    res.write(`<body> <h2>UserId: ${req.params.userId}</h2> <br> <h2>UserName: ${req.params.userName}</h2> </body>`);
+    res.write('</html>');
+    res.end();
+});
+
+
+
+
+app.listen(PORT, () => {
+    console.log(`Listening on ${PORT}`);
+})
