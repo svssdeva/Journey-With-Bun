85f97cbfed20f62226cd0e95d016bfca6b00137a
Deveshwar Jaiswal <deveshwar.jaiswal996@gmail.com>
2024-04-28 17:18:16 +0530

    feat: simple SQlite

--- bun-sqlite/index.ts
+++ bun-sqlite/index.ts
@@ -1,1 +1,7 @@
-console.log("Hello via Bun!");
+import { Database } from "bun:sqlite";
+const database = new Database("newdb.sqlite");
+database.run("CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT)");
+//database.run("INSERT INTO users (name) VALUES (?)", ["John"]);
+
+const users = database.query("SELECT * FROM users").all();
+console.log(users);
