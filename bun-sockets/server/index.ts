5e2f584f0ffc7cbc8cfc4f90a58f35e3cfa52c7d
Deveshwar Jaiswal <deveshwar.jaiswal996@gmail.com>
2024-05-06 21:00:29 +0530

    feat: Transfer message + Broadcast

--- bun-sockets/server/index.ts
+++ bun-sockets/server/index.ts
@@ -1,5 +1,6 @@
 import { v4 as uuidv4 } from "uuid";
 const clientMap = new Map();
+let onlineUsers = 0;
 const server = Bun.serve({
     port: 8080,
     fetch(req, server) {
@@ -12,13 +13,24 @@ const server = Bun.serve({
         open(ws) {
             const clientId = uuidv4();
             clientMap.set(ws, clientId);
+            onlineUsers++;
             console.log('client connected :' + clientId);
-            ws.send('client connected :' + String(clientId));
+            ws.send(`client connected : ${clientId}`);
+            ws.send(`online users : ${onlineUsers}`);
+            // ws.subscribe('welcome');
+            // server.publish('welcome', `Welcome to server  ${clientId}`);
+
+            ws.subscribe('usersConnected');
+            server.publish('usersConnected', `User joined the chat: ${clientId}`);
         },
         close(ws, code, reason) {
             const clientId = clientMap.get(ws) || 'unknown';
             console.log('client disconnected :' + clientId);
             clientMap.delete(ws);
+            onlineUsers--;
+            ws.send(`online users : ${onlineUsers}`);
+            server.publish('usersConnected', `User left the chat: ${clientId}`);
+            //  ws.unsubscribe('welcome');
         },
         message(ws, message) {
             console.log(message);
