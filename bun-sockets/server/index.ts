ca93f593c75c249b8c4ca1ced51a5419b9947080
Deveshwar Jaiswal <deveshwar.jaiswal996@gmail.com>
2024-05-06 20:48:36 +0530

    feat: Client connect + disconnect

--- bun-sockets/server/index.ts
+++ bun-sockets/server/index.ts
@@ -1,17 +1,24 @@
+import { v4 as uuidv4 } from "uuid";
+const clientMap = new Map();
 const server = Bun.serve({
     port: 8080,
     fetch(req, server) {
         if (server.upgrade(req)) {
             return;
         }
-        return new Response(`Welcome to server`);
+        return new Response(Bun.file('./public/index.html'));
     },
     websocket: {
         open(ws) {
-            console.log('client connected');
+            const clientId = uuidv4();
+            clientMap.set(ws, clientId);
+            console.log('client connected :' + clientId);
+            ws.send('client connected :' + String(clientId));
         },
         close(ws, code, reason) {
-            console.log('client disconnected');
+            const clientId = clientMap.get(ws) || 'unknown';
+            console.log('client disconnected :' + clientId);
+            clientMap.delete(ws);
         },
         message(ws, message) {
             console.log(message);
