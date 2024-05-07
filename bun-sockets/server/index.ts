c614e562492b8660b292278dff43587a1d9eea2c
Deveshwar Jaiswal <deveshwar.jaiswal996@gmail.com>
2024-05-07 20:47:13 +0530

    feat: Broadcasting, event acknowledgement, displaying connected users

--- bun-sockets/server/index.ts
+++ bun-sockets/server/index.ts
@@ -1,10 +1,12 @@
 import { v4 as uuidv4 } from "uuid";
 const clientMap = new Map();
 let onlineUsers = 0;
-const server = Bun.serve({
+const server = Bun.serve<{ username: string }>({
     port: 8080,
     fetch(req, server) {
-        if (server.upgrade(req)) {
+        if (server.upgrade(req, {
+            data: { username: `User_${Math.random().toString(16).slice(12)}` }
+        })) {
             return;
         }
         return new Response(Bun.file('./public/index.html'));
@@ -12,28 +14,53 @@ const server = Bun.serve({
     websocket: {
         open(ws) {
             const clientId = uuidv4();
-            clientMap.set(ws, clientId);
+            const username = ws.data.username;
+            clientMap.set(ws, { clientId, username });
             onlineUsers++;
             console.log('client connected :' + clientId);
-            ws.send(`client connected : ${clientId}`);
+            ws.send(`client connected : ${username}`);
             ws.send(`online users : ${onlineUsers}`);
             // ws.subscribe('welcome');
             // server.publish('welcome', `Welcome to server  ${clientId}`);
 
             ws.subscribe('usersConnected');
-            server.publish('usersConnected', `User joined the chat: ${clientId}`);
+            server.publish('usersConnected', `User joined the chat: ${username}`);
+            ws.subscribe('broadcastMsg');
+
+            const clientInfoMessage = { clientId, username };
+            ws.send(JSON.stringify(clientInfoMessage));
+
+            const userList = Array.from(clientMap.values()).map((info) => info.username);
+            ws.subscribe('usersList');
+            server.publish('usersList', JSON.stringify(userList));
         },
         close(ws, code, reason) {
-            const clientId = clientMap.get(ws) || 'unknown';
+            const clientInfo = clientMap.get(ws);
+            const clientId = clientInfo ? clientInfo.clientId : 'unknown';
+            const username = clientInfo ? clientInfo.username : 'unknown user';
             console.log('client disconnected :' + clientId);
             clientMap.delete(ws);
             onlineUsers--;
             ws.send(`online users : ${onlineUsers}`);
-            server.publish('usersConnected', `User left the chat: ${clientId}`);
-            //  ws.unsubscribe('welcome');
+            server.publish('usersConnected', `User left the chat: ${username}`);
+            const userList = Array.from(clientMap.values()).map((info) => info.username);
+            server.publish('usersList', JSON.stringify(userList));
         },
         message(ws, message) {
-            console.log(message);
+            const clientInfo = clientMap.get(ws);
+            const clientId = clientInfo ? clientInfo.clientId : 'unknown';
+            const username = clientInfo ? clientInfo.username : 'unknown user';
+
+            const ackMsg = ws.send('Message Delivered');
+
+            const dataToSend = `${username} - ${message}`;
+            server.publish('broadcastMsg', dataToSend);
+
+            if (ackMsg > 0) {
+                console.log(`Acknowledgement sent to client ${dataToSend}`);
+            } else {
+                console.error(`Acknowledgement not sent to client ${dataToSend}`);
+            }
         },
     }
 });
